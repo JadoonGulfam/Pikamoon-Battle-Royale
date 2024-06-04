0ccafd4d3337712f20347783ab6124a87ed06217
usman <muhammad.usman@tigercrypto.uk>
2024-06-04 15:03:19 +0500

    pushing zoom in out scroll

---
+++ Assets/CameraScrollZoom.cs
@@ -0,0 +1,34 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraScrollZoom : MonoBehaviour
+{
+    public float minFov ;
+    public float maxFov ;
+    public float sensitivity = 10f;
+    public GameObject virtualCamera;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2.0f);
+        virtualCamera = GetComponent<PlayerController>().virtualCamera;
+
+    }
+    public bool buffer;
+    public float buffer1;
+    void Update()
+    {
+        print(Input.GetAxis("Mouse ScrollWheel"));
+
+        if (virtualCamera != null )
+        {
+            float fov = virtualCamera.GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView;
+            fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+            fov = Mathf.Clamp(fov, minFov, maxFov);
+            virtualCamera.GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView = fov;
+        }
+    }
+}
