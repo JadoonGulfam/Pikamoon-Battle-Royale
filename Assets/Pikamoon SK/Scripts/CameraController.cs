cb97254a215845352df260c5cfed95db528f21a3
adnankhan15651 <162585573+adnankhan15651@users.noreply.github.com>
2024-10-28 15:56:41 +0500

    shift project to unity 6

--- Assets/Pikamoon SK/Scripts/CameraController.cs
+++ Assets/Pikamoon SK/Scripts/CameraController.cs
@@ -1,4 +1,4 @@
-using Cinemachine;
+using Unity.Cinemachine;
 using Pikamoon.Controller;
 using UnityEngine;
 namespace Pikamoon.Controller
--- Assets/SinglePlayerController.cs
+++ Assets/SinglePlayerController.cs
@@ -1,4 +1,4 @@
-using Cinemachine;
+using Unity.Cinemachine;
 using SickscoreGames.HUDNavigationSystem;
 using UnityEngine;
 using UnityEngine.UI;
