46894d1272554e2a8ed056c316abe27d66bd6aca
UsmanTiger <166367778+CharryTiger@users.noreply.github.com>
2024-10-29 16:57:12 +0500

    pushing work done

--- Assets/VoiceDetectionIndicator.cs
+++ Assets/VoiceDetectionIndicator.cs
@@ -6,26 +6,16 @@ public class VoiceDetectionIndicator : MonoBehaviour
 {
 
 
-    public GameObject voiceImageDetection;
-    private Recorder recorder;
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if(recorder == null)
-            recorder=GameManager.instance.gameObject.transform.Find("Recorder").GetComponent<Recorder>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (recorder.VoiceDetector.Detected)
-        {
-            Debug.Log("VoiceDetected");
-            voiceImageDetection.SetActive(true);
-        }
-        else {
-            Debug.Log("VoiceDetected Failed");
-            voiceImageDetection.SetActive(false);
-        }
+       
     }
 }
