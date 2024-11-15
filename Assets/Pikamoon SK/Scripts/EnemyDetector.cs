f7a4ae1395a7e5c36af5124dbad4590ea85f6788
adnankhan15651 <162585573+adnankhan15651@users.noreply.github.com>
2024-11-15 11:04:23 +0500

    revert branch
    .

--- Assets/Pikamoon SK/Scripts/EnemyDetector.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class EnemyDetector : MonoBehaviour
-{
-    public float Range;
-    public LayerMask LayerMask;
-
-}
--- Assets/Pikamoon SK/Scripts/Interfaces/IDamageable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface IDamageable
-{
-    void OnDamage();
-    void OnDamage(float damageAmount);
-    void OnDamage(float damageAmount, Transform hitter);
-    bool isKilled();
-}
