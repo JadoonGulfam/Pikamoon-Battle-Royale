d6c2906f4e278b025df0975ce6a553ae09e24530
UsmanTiger <muhammad.usman@tigercrypto.uk>
2024-12-23 11:53:44 +0500

    adding multiplayer abc

---
+++ Assets/PrefabSpawner.cs
@@ -0,0 +1,16 @@
+using Fusion;
+using UnityEngine;
+
+
+    public class PrefabSpawner : SimulationBehaviour, IPlayerJoined
+    {
+        public GameObject PlayerPrefab;
+
+        public void PlayerJoined(PlayerRef player)
+        {
+            if (player == Runner.LocalPlayer)
+            {
+                Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
+            }
+        }
+    }
