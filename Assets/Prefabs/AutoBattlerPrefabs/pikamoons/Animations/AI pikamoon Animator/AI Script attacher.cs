c13150e8668c2c561eca4461fe8b17ad112e484a
adnankhan15651 <162585573+adnankhan15651@users.noreply.github.com>
2025-01-06 14:53:03 +0500

    AI pikamoons(inprocess)

---
+++ Assets/Prefabs/AutoBattlerPrefabs/pikamoons/Animations/AI pikamoon Animator/AI Script attacher.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PikamoonAI))]
+[RequireComponent(typeof(BarkianPlayerFollowAI))]
+[RequireComponent(typeof(PikamoonRoaming))]
+[RequireComponent(typeof(PikamoonCommandHandler))]
+[RequireComponent(typeof(PikamoonFollow))]
+public class AIScriptattacher : MonoBehaviour
+{
+   
+}
