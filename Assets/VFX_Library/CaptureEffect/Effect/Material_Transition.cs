1c33329d9f9a0430e8e83ec26f805fe30e59be61
bismah211 <162585573+adnankhan15651@users.noreply.github.com>
2025-02-10 15:06:42 +0500

    added vfx package

---
+++ Assets/VFX_Library/CaptureEffect/Effect/Material_Transition.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Material_Transition : MonoBehaviour
+{
+    public Material material;
+    private float dissolve = 4;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        material = GetComponent<SkinnedMeshRenderer>().material;
+        material.SetFloat("_Cutoff", dissolve);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            dissolve -= 0.01f;
+            material.SetFloat("_Cutoff", dissolve);
+        }
+    }
+}
