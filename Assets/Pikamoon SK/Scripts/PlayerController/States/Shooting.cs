34b20f5ad43ebee1a1b541c62fa535485a4f3743
shaourkhanOC <shaour.khan@tigercrypto.uk>
2025-02-21 16:06:51 +0500

    bugsSolv

--- Assets/Pikamoon SK/Scripts/PlayerController/States/Shooting.cs
+++ Assets/Pikamoon SK/Scripts/PlayerController/States/Shooting.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
-using static UnityEditorInternal.ReorderableList;
 
 namespace Pikamoon.Controller
 {
