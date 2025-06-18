4b517b197adba8176cf052cb757dd52173216c9c
Shaour-OC <shaour.khan@tigercrypto.uk>
2025-06-18 14:27:57 +0500

    TagSetting SK Side

---
+++ Assets/CharacterCreatorEditor.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+namespace Pikamoon.Controller
+{
+
+    public class CharacterCreatorEditor : MonoBehaviour
+    {
+        public CharacterController referenceCharacter;
+        public CharacterController newController;
+
+
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
