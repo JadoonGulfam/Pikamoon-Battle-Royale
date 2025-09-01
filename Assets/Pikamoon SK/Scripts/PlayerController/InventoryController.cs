d9bd7ac0a0b555c86bc4051270352039f88bbc33
adnankhan15651 <162585573+adnankhan15651@users.noreply.github.com>
2025-09-01 11:56:31 +0500

    working on health syncing (Inprocess)

--- Assets/Pikamoon SK/Scripts/PlayerController/InventoryController.cs
+++ Assets/Pikamoon SK/Scripts/PlayerController/InventoryController.cs
@@ -210,7 +210,7 @@ namespace Pikamoon.Controller
                         isPickableAnItem = true;
                         pickableItem.TryToPick(this);
                         hitTransform.gameObject.SetActive(false);
-                        NetworkManagerob.GetComponent<NetworkManager>().SpawnWeapon(1);
+                        NetworkManagerob.GetComponent<NetworkManager>().SpawnWeapon(0);
                         animationController.RequestToDespawn(hitTransform.GetComponent<NetworkObject>().Id); 
                         //RequestDespawn(hitTransform.GetComponent<NetworkObject>().Id);
 
